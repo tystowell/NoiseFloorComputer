90dc548616e1c569c1d9f5a126b2b4e5f6c6cd22
Tyler Stowell <tylerrstowell@gmail.com>
2021-02-21 16:03:24 -0700

    Add options to view if a queue is empty

--- Queue.java
+++ Queue.java
@@ -1,3 +1,5 @@
+package welch;
+
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
@@ -7,19 +9,27 @@ public class Queue {
 	public static BlockingQueue<double[][]> dataQueue = new ArrayBlockingQueue<>(1);
 	public static BlockingQueue<double[]> resultQueue = new ArrayBlockingQueue<>(1);
 
-	public static void insertIntoDataQueue(double[][] x) {
+	public static void insertIntoDataQueue(double[][] x) throws InterruptedException {
 		dataQueue.put(x);
 	}
 
 	public static double[][] getFromDataQueue() throws InterruptedException {
 		return dataQueue.take();
 	}
+	
+	public static boolean dataQueueEmpty() {
+		return dataQueue.isEmpty();
+	}
 
 	public static void insertIntoResultQueue(double[] x) throws InterruptedException {
 		resultQueue.put(x);
 	}
 
-	public static double[] getFromResultQueue() {
+	public static double[] getFromResultQueue() throws InterruptedException {
 		return resultQueue.take();
+	}
+	
+	public static boolean resultQueueEmpty() {
+		return resultQueue.isEmpty();
 	}
 }
