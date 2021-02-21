cf8ea0ab13ff1ac4409a9b9d1bf39cdff051722e
Tyler Stowell <tylerrstowell@gmail.com>
2021-02-21 16:05:29 -0700

    Update WelchAccumulator.java

--- WelchAccumulator.java
+++ WelchAccumulator.java
@@ -1,4 +1,7 @@
-import java.lang.Math;
+package welch;
+
+import welch.WelchComputerThread;
+import welch.Queue;
 
 class WelchAccumulator {
 	private int segLength;
@@ -29,6 +32,7 @@ class WelchAccumulator {
 		this.result = new double[this.resultLength];
 
 		wct = new WelchComputerThread(this.segLength, this.frameSize, window);
+		wct.start();
 	}
 
 	public void close() {
@@ -40,12 +44,20 @@ class WelchAccumulator {
 		for (int i = 0; i < this.frameSize; i++)
 			for (int j = 0; j < this.segLength; j++)
 				flattenedData[i][j] = this.data[i][(this.position + j) % this.segLength];
-
-		Queue.insertIntoDataQueue(flattenedData);
+		
+		try {
+			Queue.insertIntoDataQueue(flattenedData);
+		} catch(InterruptedException e) {
+			System.out.println("uh oh");
+		}
 	}
 
-	private void getDataFromQueue() {
-		this.result = Queue.getFromResultQueue();
+	private void getResultFromQueue() {
+		try {
+			this.result = Queue.getFromResultQueue();
+		} catch(InterruptedException e) {
+			System.out.println("uh oh");
+		}
 	}
 
 	// Takes a frame and adds it to this class's buffer.
@@ -55,14 +67,27 @@ class WelchAccumulator {
 			return -1;
 
 		for (int i = 0; i < this.frameSize; i++)
-			this.data[this.position][i] = frame[i];
+			this.data[i][this.position] = frame[i];
 
 		this.position = (this.position + 1) % this.segLength;
 		this.remaining -= 1;
-
+		
 		if (this.remaining <= 0) {
 			addDataToQueue();
 			this.remaining += this.segLength - this.segOverlap;
 		}
+
+		return 0;
+	}
+	
+	public boolean resultAvailable() {
+		return !Queue.resultQueueEmpty();
+	}
+	
+	public double[] getResult() {
+		if (!resultAvailable()) return null;
+		
+		getResultFromQueue();
+		return this.result;
 	}
 }
