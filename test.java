2e3b669b06a880f8d392d35dc4ac3ef1f7a03bac
Tyler Stowell <tylerrstowell@gmail.com>
2021-03-07 14:14:45 -0700

    Update test.java

--- test.java
+++ test.java
@@ -8,24 +8,22 @@ class test {
 		int segLength  = 128;
 		int segOverlap = 64;
 		int frameSize = 100;
-		double[] window = new double[segLength];
 
-		for (int i = 0; i < segLength; i++)
-			window[i] = 0.54 - 0.46 * (double) Math.cos(2 * Math.PI * i / (segLength - 1));
-
-		WelchAccumulator w = new WelchAccumulator(segLength, segOverlap, frameSize, window);
+		WelchAccumulator w = new WelchAccumulator(segLength, segOverlap, frameSize, null);
 
 		double[] data = new double[frameSize];
-		double[] result = new double[segLength / 2 + 1];
+		double result;
 
-		for (int i = 0; i < 256; i++) {
+		for (int i = 0; i < 2048; i++) {
 			for (int n = 0; n < frameSize; n++)
 				data[n] = Math.random() * 30;
 			
 			w.addFrame(data);
 			
-			if (w.resultAvailable())
+			if (w.resultAvailable()) {
 				result = w.getResult();
+				System.out.println("Result: " + result);
+			}
 		}
 		w.close();
 	}
