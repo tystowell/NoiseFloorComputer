b7749d5be6bd84bd172f430610daf11641ab62e3
Tyler Stowell <tylerrstowell@gmail.com>
2021-03-07 16:24:37 -0700

    Added WelchIntegratorThread's functionality
    I was encountering a rough bug where the array that this thread pushed to WelchIntegrator was showing up as being filled with all 0s. After some investigation, I realized that ArrayBlockingQueues works with pointers, and not the data values themselves (which makes sense, since threads usually share the same memory space). However, at some point, WelchComputerThread had to overwrite the values in this.periodogram, and if WelchIntegrator wasn't fast enough to catch them, it would get garbage data. To fix this race condition, I added the computations of WelchIntegrator into WelchComputer, since the extra thread wasn't really necessary anyways (it does several orders of magnitude less work than WelchComputer already did), and the alternative was to have WelchComputer block until WelchIntegrator was done, which would slow down the thread that needs to be the fastest.

--- WelchComputerThread.java
+++ WelchComputerThread.java
@@ -5,18 +5,24 @@ import java.lang.Math;
 import org.jtransforms.fft.*;
 
 class WelchComputerThread extends Thread {
+	private static final double NOISE_EST_PERCENT = 0.2;  // The percent of the periodogram (on the end) to use for the noise estimation.
+	private static final int SAMPLE_RATE = 1970; // The sample rate in Hz
+	private static final double SCALE_FACTOR = 225.438;
+	
 	private int segLength;
 	private int frameSize;
 	private double[] window;
 
-	private int resultLength;
 	private double scale;
 
 	private double[][] data;
 
 	private DoubleFFT_1D fft;
 
-	public double[] result;
+	private int fourierLength;
+	public double[] periodogram;
+	
+	double result;
 
 	public WelchComputerThread(int segLength, int frameSize, double[] window) {
 		this.segLength = segLength;
@@ -34,14 +40,14 @@ class WelchComputerThread extends Thread {
 	    		this.window[i] = 1; // If window is the incorrect length, don't use a window
 	    }
 		
-		this.resultLength = (int) (this.segLength / 2 + 1);
+		this.fourierLength = (int) (this.segLength / 2 + 1);
 		setPeriodogramScale();
 
 		this.data = new double[this.frameSize][this.segLength];
 
 		fft = new DoubleFFT_1D(this.segLength);
 
-		this.result = new double[this.resultLength];
+		this.periodogram = new double[this.fourierLength];
 	}
 
 	// Sets the scale for computing the periodogram.
@@ -74,7 +80,7 @@ class WelchComputerThread extends Thread {
 	// 2) Scaling (most) values by 2 to conserve the energy
 	// 3) Scaling by this.scale
 	private void realSpectrumToPeriodogram(double[] spectrum, double[] periodogram) {
-		for (int i = 0; i < this.resultLength; i ++) {
+		for (int i = 0; i < this.fourierLength; i ++) {
 			periodogram[i] = 0;
 			if (this.segLength % 2 == 1) {
 				// If the signal length(n) was odd, spectrum is in the following format:
@@ -84,7 +90,7 @@ class WelchComputerThread extends Thread {
 
 				periodogram[i] += Math.pow(spectrum[2*i], 2); // Re[i]
 
-				if (i == this.resultLength - 1)
+				if (i == this.fourierLength - 1)
 					periodogram[i] += Math.pow(spectrum[1], 2); // Im[(n-1)/2]
 				else if (i != 0)
 					periodogram[i] += Math.pow(spectrum[2*i+1], 2); // Im[i]
@@ -97,12 +103,12 @@ class WelchComputerThread extends Thread {
 				// a[2i+1] = Im[i], 0<k<n/2
 				// a[1] = Re[n/2]
 
-				if (i == this.resultLength - 1)
+				if (i == this.fourierLength - 1)
 					periodogram[i] += Math.pow(spectrum[1], 2); // Re[n/2]
 				else
 					periodogram[i] += Math.pow(spectrum[2*i], 2); // Re[i]
 
-				if (i > 0 && i < this.resultLength - 1) {
+				if (i > 0 && i < this.fourierLength - 1) {
 					periodogram[i] += Math.pow(spectrum[2*i+1], 2); // Im[i]
 					periodogram[i] *= 2; // Preserve energy in signal
 				}
@@ -112,13 +118,14 @@ class WelchComputerThread extends Thread {
 		}
 	}
 
-	// Computes the periodogram of the last segment and stores it in this.result
-	private void computeSegment() {
+	// Computes the periodogram of the last segment and stores it in this.periodogram
+	private void computePeriodogram() {
 		double[] signal = new double[this.segLength];
-		double[] periodogram = new double[this.resultLength];
+		double[] periodogram = new double[this.fourierLength];
 
-		for (int i = 0; i < this.resultLength; i++)
-			this.result[i] = 0;
+		for (int i = 0; i < this.fourierLength; i++) {
+			this.periodogram[i] = 0;
+		}
 
 		for (int i = 0; i < this.frameSize; i++) {
 			createSignal(signal, i);
@@ -126,17 +133,31 @@ class WelchComputerThread extends Thread {
 			fft.realForward(signal);
 			realSpectrumToPeriodogram(signal, periodogram);
 
-			for (int j = 0; j < this.resultLength; j++)
-				this.result[j] += periodogram[j] / this.frameSize;
+			for (int j = 0; j < this.fourierLength; j++)
+				this.periodogram[j] += periodogram[j] / this.frameSize;
 		}
 	}
 	
-	private boolean addPeriodogramToQueue() {
+	private void computeNoiseFloor() {
+		int noiseEstimateLength = (int) (this.fourierLength * WelchComputerThread.NOISE_EST_PERCENT);
+		double noiseFloor = 0;
+		if (noiseEstimateLength >= 1) {
+			for (int i = this.fourierLength - 1; i >= this.fourierLength - noiseEstimateLength; i--) {
+				noiseFloor += this.periodogram[i];
+			}
+			noiseFloor /= noiseEstimateLength;
+		}else {
+			noiseFloor = this.periodogram[this.fourierLength];
+		}
+		this.result = WelchComputerThread.SCALE_FACTOR * noiseFloor * (WelchComputerThread.SAMPLE_RATE / 2);
+	}
+	
+	private boolean addResultToQueue() {
 		if (Queue.done)
 			return false;
 		
 		try {
-			return Queue.insertIntoPeriodogramQueue(this.result);
+			return Queue.insertIntoResultQueue(this.result);
 		} catch (InterruptedException e) {
 			Queue.done = true;
 			return false;
@@ -162,13 +183,15 @@ class WelchComputerThread extends Thread {
 	}
 
 	public void run() {
-			while (!Queue.done) {
-				if (setDataFromQueue())
-				{
-					computeSegment();
-					
-					addPeriodogramToQueue();
-				}
+		while (!Queue.done) {
+			if (setDataFromQueue())
+			{
+				computePeriodogram();
+				
+				computeNoiseFloor();
+				
+				addResultToQueue();
 			}
+		}
 	}
 }
